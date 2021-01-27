baa92df592c0bb5e9a5a288ed03e4e18b724b8b7
Chany <inshakr2@naver.com>
2021-01-28 00:47:12 +0900

    Add JavaDoc to Patient, VisitServiceImpl

--- MedicalRecord/src/main/java/chany/task/service/PatientServiceImpl.java
+++ MedicalRecord/src/main/java/chany/task/service/PatientServiceImpl.java
@@ -14,27 +14,50 @@ public class PatientServiceImpl implements PatientService{
         this.patientRepository = patientRepository;
     }
 
+    /**
+     * 입력받는 Patient 등록
+     * @param patient
+     * @return 등록된 patientId
+     */
     @Override
     public Long regist(Patient patient) {
         patientRepository.save(patient);
         return patient.getPatientId();
     }
 
+    /**
+     * 모든  Patient List 조회
+     * @return Patient List
+     */
     @Override
     public List<Patient> findAll() {
         return patientRepository.findAll();
     }
 
+    /**
+     * ID를 입력 값으로 Patient 조회
+     * @param patientId
+     * @return 조회 결과 Patient
+     */
     @Override
     public Optional<Patient> findOne(Long patientId) {
         return patientRepository.findById(patientId);
     }
 
+    /**
+     * ID를 입력 값으로 Patient 삭제
+     * @param patientId
+     */
     @Override
     public void deleteOne(Long patientId) {
         patientRepository.deleteById(patientId);
     }
 
+    /**
+     * ID와 새로운 Patient를 입력받아 입력받은 Patient의 Data를 Set
+     * @param patientId
+     * @param newPatient
+     */
     @Override
     public void modifyOne(Long patientId, Patient newPatient) {
         Optional<Patient> oldPatient = patientRepository.findById(patientId);
--- MedicalRecord/src/main/java/chany/task/service/VisitServiceImpl.java
+++ MedicalRecord/src/main/java/chany/task/service/VisitServiceImpl.java
@@ -14,28 +14,51 @@ public class VisitServiceImpl implements VisitService{
         this.visitRepository = visitRepository;
     }
 
+    /**
+     * 입력받는 Visit 등록
+     * @param visit
+     * @return 등록된 VisitId
+     */
     @Override
     public Long regist(Visit visit) {
         visitRepository.save(visit);
         return visit.getVisitId();
     }
 
+    /**
+     * 모든 Visit List 조회
+     * @return Visit List
+     */
     @Override
     public List<Visit> findAll() {
         return visitRepository.findAll();
     }
 
+    /**
+     * ID를 입력 값으로 Visit 조회
+     * @param visitId
+     * @return 조회결과 Visit
+     */
     @Override
     public Optional<Visit> findOne(Long visitId) {
         return visitRepository.findById(visitId);
     }
 
+    /**
+     * ID를 입력 값으로 Visit 삭제
+     * @param visitId
+     */
     @Override
     public void deleteOne(Long visitId) {
         visitRepository.deleteById(visitId);
 
     }
 
+    /**
+     * ID와 새로운 Visit를 입력받아 입력받은 Visit의 Data를 Set
+     * @param visitId
+     * @param newVisit
+     */
     @Override
     public void modifyOne(Long visitId, Visit newVisit) {
         Optional<Visit> oldVisit = visitRepository.findById(visitId);
