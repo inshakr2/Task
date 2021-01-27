7376ab7802a14c4df05f3ac672b0dd78b02c57c5
Chany <inshakr2@naver.com>
2021-01-28 00:22:25 +0900

    Set omitted @Repository to Repositories

--- MedicalRecord/src/main/java/chany/task/repository/PatientRepository.java
+++ MedicalRecord/src/main/java/chany/task/repository/PatientRepository.java
@@ -2,6 +2,8 @@ package chany.task.repository;
 
 import chany.task.domain.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 }
--- MedicalRecord/src/main/java/chany/task/repository/VisitRepository.java
+++ MedicalRecord/src/main/java/chany/task/repository/VisitRepository.java
@@ -2,6 +2,8 @@ package chany.task.repository;
 
 import chany.task.domain.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VisitRepository extends JpaRepository<Visit, Long> {
 }
