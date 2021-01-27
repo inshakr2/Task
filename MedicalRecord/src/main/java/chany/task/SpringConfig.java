81c4217a4be535320dd22f2a3720786cd6dba014
Chany <inshakr2@naver.com>
2021-01-28 00:20:40 +0900

    Add spring bean dependency configuration (visit, patient service) to spring config

--- MedicalRecord/src/main/java/chany/task/SpringConfig.java
+++ MedicalRecord/src/main/java/chany/task/SpringConfig.java
@@ -2,8 +2,15 @@ package chany.task;
 
 import chany.task.repository.PatientRepository;
 import chany.task.repository.VisitRepository;
+import chany.task.service.PatientService;
+import chany.task.service.PatientServiceImpl;
+import chany.task.service.VisitService;
+import chany.task.service.VisitServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SpringConfig {
     private PatientRepository patientRepository;
     private VisitRepository visitRepository;
@@ -12,5 +19,15 @@ public class SpringConfig {
     public SpringConfig(PatientRepository patientRepository, VisitRepository visitRepository) {
         this.patientRepository = patientRepository;
         this.visitRepository = visitRepository;
+    }
+
+    @Bean
+    public PatientService patientService() {
+        return new PatientServiceImpl(patientRepository);
+    }
+
+    @Bean
+    public VisitService visitService() {
+        return new VisitServiceImpl(visitRepository);
     }
 }
