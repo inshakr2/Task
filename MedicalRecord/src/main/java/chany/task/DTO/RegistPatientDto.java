ae9c374bf13b116db23ca64ddc8aaa63360e54bd
Chany <inshakr2@naver.com>
2021-02-03 21:52:39 +0900

    Definition RegistDto

--- MedicalRecord/src/main/java/chany/task/DTO/RegistPatientDto.java
+++ MedicalRecord/src/main/java/chany/task/DTO/RegistPatientDto.java
@@ -1,4 +1,24 @@
 package chany.task.DTO;
 
 public class RegistPatientDto {
+    /**
+     * name
+     * registNo
+     * gendercode
+     * visitDay
+     * visitcode
+     */
+
+    private String patientName;
+
+    private String registNumber;
+
+    private String genderCode;
+
+    private String birth;
+
+    private String phoneNumber;
+
+    private String visitCode;
+
 }
