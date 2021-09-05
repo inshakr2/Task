23784b33df1ed365b53ae46a282889bcebc6233a
Chany <inshakr2@naver.com>
2021-09-05 15:40:58 +0900

    Task 3rd - 1. 웹 프로젝트 구성

---
+++ MedicalRecord3/src/main/java/chany/task/MedicalRecord3/MedicalRecord3Application.java
@@ -0,0 +1,13 @@
+package chany.task.MedicalRecord3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRecord3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRecord3Application.class, args);
+	}
+
+}
---
+++ MedicalRecord3/src/test/java/chany/task/MedicalRecord3/MedicalRecord3ApplicationTests.java
@@ -0,0 +1,13 @@
+package chany.task.MedicalRecord3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecord3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
