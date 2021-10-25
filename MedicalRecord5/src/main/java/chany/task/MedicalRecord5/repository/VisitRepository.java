5de73b5f143189edf037aa29df1c46150032a3ce
Chany <inshakr2@naver.com>
2021-10-25 19:48:40 +0900

    Task - 0. fix Repository

--- MedicalRecord5/src/main/java/chany/task/MedicalRecord5/repository/VisitRepository.java
+++ MedicalRecord5/src/main/java/chany/task/MedicalRecord5/repository/VisitRepository.java
@@ -3,6 +3,7 @@ package chany.task.MedicalRecord5.repository;
 import chany.task.MedicalRecord5.domain.Visit;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +11,7 @@ import java.util.Optional;
 @Repository
 public interface VisitRepository extends JpaRepository<Visit, Long> {
     @Override
-    @EntityGraph(attributePaths = {"hospital"})
+    @Query("SELECT v FROM Visit v " +
+            "JOIN FETCH v.hospital")
     Optional<Visit> findById(Long aLong);
 }
